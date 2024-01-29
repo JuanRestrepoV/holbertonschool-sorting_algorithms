83a6d3c8f70db03c2aeade71994cbf39ca36e7d8
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-01-29 14:43:50 -0800

    update

---
+++ sort.h
@@ -0,0 +1,10 @@
+#ifndef _SORTING_ALG_
+#define _SORTING_ALG_
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif
